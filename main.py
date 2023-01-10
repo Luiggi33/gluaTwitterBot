0db2b2ffd2927a1c3b2b0c76664ee89d16a76760
Aron Rohr <aron.rohr@web.de>
2023-01-10 21:04:54 +0100

    updated to usable version

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import json
+import random
+import time
 import tweepy
 import os
 import requests
 from dotenv import load_dotenv
+import schedule
+from bs4 import BeautifulSoup
+import re
 
 load_dotenv()
 
@@ -16,13 +22,45 @@ auth = tweepy.OAuth1UserHandler(
 )
 
 api = tweepy.API(auth)
+pages = None
+
+
+def tweet_random_page():
+    random_page_num = random.randint(0, len(pages))
+    page = pages[random_page_num]
+    url = "https://wiki.facepunch.com/gmod/" + page["address"]
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.content, 'html.parser')
+    tweet_title = "not real"
+
+    tweet = "Today's wiki page is "
+
+    for title in soup.find_all("title"):
+        tweet_title = re.match(r"(.*?)\s-", title.get_text()).group(1)
+        tweet = tweet + tweet_title + "\n"
+
+    description_section = soup.find("div", class_="description_section")
+    if description_section:
+        for tag in description_section(["a", "p"]):
+            tag.unwrap()
+        tweet = tweet + description_section.text
+
+    api.update_status(tweet + url)
+    pages.remove(page)
+
+    print("Tweeted tweet: " + tweet_title)
+
 
 if __name__ == "__main__":
-   pages = requests.get("https://wiki.facepunch.com/gmod/~pagelist?format=json")
+    req = requests.get("https://wiki.facepunch.com/gmod/~pagelist?format=json")
+    pages = req.json()
 
-   f = open("pages.json")
-   cached_pages = f.readlines()
+    with open('pages.json', 'w') as file:
+        file.write(json.dumps(pages))
 
-   print(type(pages))
-   #if pages != cached_pages:
+    tweet_random_page()
+    schedule.every(12).hours.do(tweet_random_page)
 
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
